983a097598693788ab000c926e24df8f2eaefa27
alexlam184 <alexwork0211@gmail.com>
2023-07-25 22:00:09 +0800

    feat : add calendar

--- src/app/[locale]/calendar/page.tsx
+++ src/app/[locale]/calendar/page.tsx
@@ -1,8 +1,36 @@
+"use client";
 import React from "react";
 import PageLayout from "@src/components/common/PageLayout";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+
+const events = [{ title: "Meeting", start: new Date() }];
+
+function renderEventContent(eventInfo) {
+  return (
+    <>
+      <b>{eventInfo.timeText}</b>
+      <i>{eventInfo.event.title}</i>
+    </>
+  );
+}
 
 const Calendar = () => {
-  return <PageLayout>calendar</PageLayout>;
+  return (
+    <PageLayout>
+      {" "}
+      <div>
+        <h1>Demo App</h1>
+        <FullCalendar
+          plugins={[dayGridPlugin]}
+          initialView="dayGridMonth"
+          weekends={false}
+          events={events}
+          eventContent={renderEventContent}
+        />
+      </div>
+    </PageLayout>
+  );
 };
 
 export default Calendar;
