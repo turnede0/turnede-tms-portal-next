12faefa64cf16359c3d705075b99359ce75d0a03
turnede0 <turnede0@gmail.com>
2023-08-02 20:52:53 +0800

    test : build2

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 export default function Home() {
   useEffect(() => {
     console.log("alex key", process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY);
+    console.log("alex key", process.env.CLERK_SECRET_KEY);
   }, []);
 
   return (
