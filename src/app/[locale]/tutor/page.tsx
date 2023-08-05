fed966cb0bff7df0d2aa83095549761c2b2cc6e3
turnede0 <turnede0@gmail.com>
2023-08-05 21:17:22 +0800

    add page

---
+++ src/app/[locale]/tutor/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import PageLayout from "@src/components/common/PageLayout";
+
+const Tutor = () => {
+  return <PageLayout>Tutor</PageLayout>;
+};
+
+export default Tutor;
