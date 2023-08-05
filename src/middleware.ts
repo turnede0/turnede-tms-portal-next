4b4fce0e2d8c2ef21578e631537bf5835bd70648
turnede0 <turnede0@gmail.com>
2023-08-05 18:36:30 +0800

    change middlware config

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,20 @@
 import { authMiddleware } from "@clerk/nextjs";
-// import createMiddleware from "next-intl/middleware";
+import createMiddleware from "next-intl/middleware";
 
-// const intlMiddleware = createMiddleware({
-//   locales: ["en", "zh"],
-//   defaultLocale: "en",
-// });
+const intlMiddleware = createMiddleware({
+  locales: ["en", "zh"],
+  defaultLocale: "en",
+});
 
-// export default authMiddleware({
-//   beforeAuth: (req) => {
-//     return intlMiddleware(req);
-//   },
+export default authMiddleware({
+  beforeAuth: (req) => {
+    return intlMiddleware(req);
+  },
 
-//   publicRoutes: ["/", "/:locale/sign-in"],
-// });
+  publicRoutes: ["/"],
+});
 
-export default authMiddleware();
+// export default authMiddleware();
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
